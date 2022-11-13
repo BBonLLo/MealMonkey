43a1c583339bc0abd239886ab84a95a30d582957
BBonLLo <65909299+BBonLLo@users.noreply.github.com>
2022-11-13 22:09:05 +0100

    Animation and Reconfigurate Feed

--- app/src/main/java/com/example/mealmonkey/About.java
+++ app/src/main/java/com/example/mealmonkey/About.java
@@ -14,6 +14,7 @@ import android.widget.VideoView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class About extends AppCompatActivity {
+
     private VideoView videoView;
 
     @Override
